2a3890b0c6998445336d73f5c1e237dd33920ff4
al-Bala <aleksandra.bala.01@gmail.com>
2024-11-30 17:44:41 +0100

    Added controller data validation, corrected api error handling

--- src/main/java/com/weatherappbackend/clientweatherapi/error/ClientErrorHandler.java
+++ src/main/java/com/weatherappbackend/clientweatherapi/error/ClientErrorHandler.java
@@ -1,30 +1,31 @@
 package com.weatherappbackend.clientweatherapi.error;
 
-import com.weatherappbackend.clientweatherapi.ClientService;
+import com.weatherappbackend.weather.controller.WeatherController;
 import feign.FeignException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
-@ControllerAdvice(assignableTypes = ClientService.class)
+@ControllerAdvice(assignableTypes = WeatherController.class)
 public class ClientErrorHandler {
 
     @ExceptionHandler(FeignException.class)
     @ResponseBody
-    public ErrorClientResponse handleApiError(FeignException exception) {
-        if(exception.getCause() instanceof FeignException.FeignClientException){
+    public ResponseEntity<String> handleApiError(FeignException exception) {
+        if(exception instanceof FeignException.FeignClientException){
             log.error("Client exception: {}", exception.status());
         }
-        if(exception.getCause() instanceof FeignException.FeignServerException){
+        else if(exception instanceof FeignException.FeignServerException){
             log.error("Server exception: {}", exception.status());
         }
-        if(exception.getCause() instanceof FeignException){
+        else {
             log.error("Feign exception: {} {}", exception.getMessage(), exception.status());
         }
-        return new ErrorClientResponse(exception.getMessage(), HttpStatus.valueOf(exception.status()));
+        return ResponseEntity.status(HttpStatus.valueOf(exception.status())).body(exception.getMessage());
     }
 
 }
--- src/main/java/com/weatherappbackend/weather/controller/WeatherController.java
+++ src/main/java/com/weatherappbackend/weather/controller/WeatherController.java
@@ -4,11 +4,11 @@ import com.weatherappbackend.clientweatherapi.ClientService;
 import com.weatherappbackend.clientweatherapi.response.forecast.ForecastClientResponse;
 import com.weatherappbackend.clientweatherapi.response.summary.SummaryClientResponse;
 import com.weatherappbackend.weather.Weather;
+import com.weatherappbackend.weather.WeatherMapper;
 import com.weatherappbackend.weather.forecast.ForecastDayDto;
 import com.weatherappbackend.weather.forecast.ForecastWeather;
-import com.weatherappbackend.weather.weeksummary.SummaryWeather;
 import com.weatherappbackend.weather.weeksummary.SummaryDto;
-import com.weatherappbackend.weather.WeatherMapper;
+import com.weatherappbackend.weather.weeksummary.SummaryWeather;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,10 +25,14 @@ public class WeatherController {
 
     @GetMapping("/weather-forecast/{latitude}/{longitude}")
     public ResponseEntity<List<ForecastDayDto>> getWeatherForecast(
-            @PathVariable double latitude,
-            @PathVariable double longitude
+            @PathVariable String latitude,
+            @PathVariable String longitude
     ) {
-        ForecastClientResponse forecastClientResponse = clientService.findForecastWeather(latitude, longitude);
+        if(isCoordinatesValid(latitude, longitude)) {
+            return ResponseEntity.badRequest().build();
+        }
+        ForecastClientResponse forecastClientResponse =
+                clientService.findForecastWeather(Double.parseDouble(latitude), Double.parseDouble(longitude));
         ForecastWeather forecastWeather = WeatherMapper.mapFromResponseToForecastWeather(forecastClientResponse);
         List<ForecastDayDto> weatherForecast = weather.getWeatherForecast(forecastWeather);
         return ResponseEntity.ok(weatherForecast);
@@ -36,13 +40,21 @@ public class WeatherController {
 
     @GetMapping("/week-summary/{latitude}/{longitude}")
     public ResponseEntity<SummaryDto> getWeekSummary(
-            @PathVariable double latitude,
-            @PathVariable double longitude
+            @PathVariable String latitude,
+            @PathVariable String longitude
     ) {
-        SummaryClientResponse summaryClientResponse = clientService.getSummaryWeather(latitude, longitude);
+        if(isCoordinatesValid(latitude, longitude)) {
+            return ResponseEntity.badRequest().build();
+        }
+        SummaryClientResponse summaryClientResponse =
+                clientService.getSummaryWeather(Double.parseDouble(latitude), Double.parseDouble(longitude));
         SummaryWeather summaryWeather = WeatherMapper.mapFromResponseToSummaryWeather(summaryClientResponse);
         SummaryDto summaryDto = weather.getWeekSummary(summaryWeather);
         return ResponseEntity.ok(summaryDto);
     }
 
+    private static boolean isCoordinatesValid(String latitude, String longitude) {
+        String regex = "\\d+.\\d+";
+        return !latitude.matches(regex) && !longitude.matches(regex);
+    }
 }
