e6a564b1e224dacf1ce9809725fb84fbd464e3d6
al-Bala <aleksandra.bala.01@gmail.com>
2024-11-30 23:23:32 +0100

    Updated cors config

--- src/main/java/com/weatherappbackend/CorsConfig.java
+++ src/main/java/com/weatherappbackend/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://weatherapp-frontend-z3dk.onrender.com")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
